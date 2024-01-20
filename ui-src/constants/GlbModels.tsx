a602ad468037069bee07bd65961eb6bcbf7bb757
佐藤　文彦 <fumihiko_sato@post.pioneer.co.jp>
2024-01-20 17:48:18 +0900

    追加

--- ui-src/constants/GlbModels.tsx
+++ ui-src/constants/GlbModels.tsx
@@ -1,21 +1,22 @@
 // thumb images
-import HeartThumb from "../assets/images/thumb_heart.png";
-import CowThumb from "../assets/images/thumb_cow.png";
-import MotorcycleThumb from "../assets/images/thumb_Motorcycle.png";
+// import HeartThumb from "../assets/images/thumb_heart.png";
+// import CowThumb from "../assets/images/thumb_cow.png";
+// import MotorcycleThumb from "../assets/images/thumb_Motorcycle.png";
+import TubThumb from "../assets/images/thumb_tub.png";
 
 // models
-import Heart from "../components/Heart";
-import Cow from "../components/Cow";
-import Motorcycle from "../components/Motorcycle";
-import Locker from "../components/Locker";
+// import Heart from "../components/Heart";
+// import Cow from "../components/Cow";
+// import Motorcycle from "../components/Motorcycle";
+// import Locker from "../components/Locker";
 import Tub from "../components/Tub";
 
 const GlbModels = [
-  { name: "heart", thumb: HeartThumb, component: <Heart /> },
-  { name: "cow", thumb: CowThumb, component: <Cow /> },
-  { name: "motorcycle", thumb: MotorcycleThumb, component: <Motorcycle /> },
-  { name: "locker", thumb: MotorcycleThumb, component: <Locker /> },
-  { name: "tub", thumb: MotorcycleThumb, component: <Tub /> },
+  //   { name: "heart", thumb: HeartThumb, component: <Heart /> },
+  //   { name: "cow", thumb: CowThumb, component: <Cow /> },
+  //   { name: "motorcycle", thumb: MotorcycleThumb, component: <Motorcycle /> },
+  //   { name: "locker", thumb: MotorcycleThumb, component: <Locker /> },
+  { name: "tub", thumb: TubThumb, component: <Tub /> },
 ];
 
 export default GlbModels;
