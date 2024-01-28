6b22605897fe4d0f276e04f50c422a203655d259
佐藤　文彦 <fumihiko_sato@post.pioneer.co.jp>
2024-01-28 19:44:26 +0900

    no message

--- ui-src/constants/GlbModels.tsx
+++ ui-src/constants/GlbModels.tsx
@@ -1,16 +1,10 @@
 // thumb images
-// import HeartThumb from "../assets/images/thumb_heart.png";
-// import CowThumb from "../assets/images/thumb_cow.png";
-// import MotorcycleThumb from "../assets/images/thumb_Motorcycle.png";
 import TubThumb from "../assets/images/thumb_tub.png";
 import BathChairThumb from "../assets/images/thumb_bathChair.png";
 import BathThumb from "../assets/images/thumb_bath.png";
 import LockerThumb from "../assets/images/thumb_locker.png";
 import MirrorThumb from "../assets/images/thumb_mirror.png";
 // models
-// import Heart from "../components/Heart";
-// import Cow from "../components/Cow";
-// import Motorcycle from "../components/Motorcycle";
 import Locker from "../components/Locker";
 import Tub from "../components/Tub";
 import BathChair from "../components/BathChair";
